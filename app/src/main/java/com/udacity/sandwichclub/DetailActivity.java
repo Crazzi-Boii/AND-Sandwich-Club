378c6d59d0fcce3a1596555c6043a9cd2a1ecfac
Crazzi-Boii <cool.aryo@gmail.com>
2020-06-01 15:28:38 +0530

    JSON parsing completed
    Signed-off-by: Crazzi-Boii <cool.aryo@gmail.com>

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,6 +3,7 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -14,6 +15,46 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private static Sandwich sandwich;
+
+    public static void showData() {
+        // GETTING LIST DATA
+
+        Log.e("-----------------------", "--------------------------------");
+        Log.e("showData: Main Name", "" + sandwich.getMainName());
+
+        if (sandwich.getAlsoKnownAs() != null)
+            for (String a : sandwich.getAlsoKnownAs())
+                Log.e("showData: Also Known As", "" + a);
+        else
+            Log.e("showData: Also Known As", "null value");
+
+        if (sandwich.getPlaceOfOrigin() != null)
+            Log.e("showData: placeOfOrigin", "" + sandwich.getPlaceOfOrigin());
+
+        if (sandwich.getDescription() != null)
+            Log.e("showData: Description", "" + sandwich.getDescription());
+
+        if (sandwich.getImage() != null)
+            Log.e("showData: Image", "" + sandwich.getImage());
+
+        if (sandwich.getIngredients() != null)
+            for (String a : sandwich.getIngredients())
+                Log.e("showData: Ingredients", "" + a);
+        else
+            Log.e("showData: Ingredients", "null value");
+
+    }
+
+    private void closeOnError() {
+        finish();
+        Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void populateUI() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +68,7 @@ public class DetailActivity extends AppCompatActivity {
             closeOnError();
         }
 
+        assert intent != null;
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
@@ -36,7 +78,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -49,14 +91,5 @@ public class DetailActivity extends AppCompatActivity {
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
-    }
-
-    private void closeOnError() {
-        finish();
-        Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
-    }
-
-    private void populateUI() {
-
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,61 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class JsonUtils {
+    private static Sandwich sandwich = new Sandwich();
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        List<String> alsoKnownAsList = new ArrayList<>();
+        List<String> ingredientsList = new ArrayList<>();
+        try {
+            JSONObject sandwitchJobj = new JSONObject(json);
+
+            JSONObject sandwitchNameJobj = new JSONObject(sandwitchJobj.getString("name"));
+            sandwich.setMainName(sandwitchNameJobj.getString("mainName"));
+
+            String alsoKnownAs = sandwitchNameJobj.getString("alsoKnownAs");
+            if (alsoKnownAs.length() > 2) {
+                alsoKnownAs = alsoKnownAs.substring(2, alsoKnownAs.length() - 2);
+                String[] a = alsoKnownAs.split("\",\"");
+                alsoKnownAsList.addAll(Arrays.asList(a));
+                sandwich.setAlsoKnownAs(alsoKnownAsList);
+            } else {
+                sandwich.setAlsoKnownAs(null);
+            }
+
+            String placeOfOrigin = sandwitchJobj.getString("placeOfOrigin");
+            if (placeOfOrigin != null)
+                sandwich.setPlaceOfOrigin(placeOfOrigin);
+
+            String description = sandwitchJobj.getString("description");
+            if (description != null)
+                sandwich.setDescription(description);
+
+            String image = sandwitchJobj.getString("image");
+            if (image != null)
+                sandwich.setImage(image);
+
+            String ingredients = sandwitchJobj.getString("ingredients");
+            if (ingredients.length() > 2) {
+
+                ingredients = ingredients.substring(2, ingredients.length() - 2);
+                String[] a = ingredients.split("\",\"");
+                ingredientsList.addAll(Arrays.asList(a));
+                sandwich.setIngredients(ingredientsList);
+            } else {
+                sandwich.setIngredients(null);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
     }
 }
